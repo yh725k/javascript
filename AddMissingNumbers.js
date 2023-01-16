6e749101a3bad44bf300b423d1e09daf780bcb25
23yoohee <59354758+yh725k@users.noreply.github.com>
2023-01-16 10:47:45 +0900

    Create AddMissingNumbers.js

---
+++ AddMissingNumbers.js
@@ -0,0 +1,36 @@
+/*
+[문제 설명]
+0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+[제한사항]
+1 ≤ numbers의 길이 ≤ 9
+0 ≤ numbers의 모든 원소 ≤ 9
+numbers의 모든 원소는 서로 다릅니다.
+
+입출력 예numbersresult
+[1,2,3,4,6,7,8,0]	14
+[5,8,4,0,6,7,9]	6입출력 예 설명
+
+입출력 예 #1
+5, 9가 numbers에 없으므로, 5 + 9 = 14를 return 해야 합니다.
+
+입출력 예 #2
+1, 2, 3이 numbers에 없으므로, 1 + 2 + 3 = 6을 return 해야 합니다.
+*/
+
+function solution(numbers) {
+    var answer = 0;
+    const lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let newlst = lst.filter(x => !numbers.includes(x))
+
+    for (let i=0; i<newlst.length; i++) {
+        answer += newlst[i]
+    }
+    return answer;
+}
+
+/*
+function solution(numbers) {
+    return 45 - numbers.reduce((cur, acc) => cur + acc, 0);
+}
+*/
